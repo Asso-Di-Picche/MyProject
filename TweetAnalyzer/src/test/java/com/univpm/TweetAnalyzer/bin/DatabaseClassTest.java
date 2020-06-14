23c5891a4afdcc764e7d53757be40e36345b1ae6
antonet <antoniobaio90@gmail.com>
2020-06-14 11:31:05 +0200

    aggiunte annotazioni sui test, cambiato metodo di assert

--- TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/bin/DatabaseClassTest.java
+++ TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/bin/DatabaseClassTest.java
@@ -1,17 +1,22 @@
 package com.univpm.TweetAnalyzer.bin;
 
 import com.univpm.TweetAnalyzer.model.Data;
+import org.hamcrest.collection.IsMapContaining;
 import org.junit.Before;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
 
-import com.univpm.TweetAnalyzer.bin.DatabaseClass;
+
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,20 +24,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class DatabaseClassTest {
 
     DatabaseClass data;
-    private static final String path = "/Users/antoniobaio/Desktop/MyProject/TweetAnalyzer/Files/original_hashtags2.txt";
+    private static final String path = "/Users/antoniobaio/Desktop/MyProject/TweetAnalyzer/Files/original_hashtags.txt";
     private static String in = null;
     private static ArrayList<String> hashtag = new ArrayList<String>();
-    private static String[] hash = {"Prada", "Armani",
-            "Ferragamo",
-            "Gucci",
-            "Versace",
-            "Trussardi",
-            "Valentino",
-            "Zegna",
-            "Dior",
-            "Benetton"};
+    private static String[] hashArray = {"Prada", "Armani", "Ferragamo", "Gucci", "Versace", "Trussardi", "Valentino", "Zegna", "Dior", "Benetton"};
 
     @Test
+    @DisplayName("Test simulazione caricamento hashtag")
+    @Tag("hashtag")
     void hashtagObtainTest() throws FileNotFoundException {
 
         Scanner in = new Scanner(new BufferedReader(new FileReader(path)));
@@ -41,12 +40,13 @@ class DatabaseClassTest {
 
             hashtag.add(in.next());
         }
-        in.close();
 
-        for (int i = 0; i < hashtag.size(); i++) {
+        if (in != null) {
 
-            assertEquals(hash[i], hashtag.get(i));
+            in.close();
         }
+
+        assertIterableEquals(Arrays.asList(hashArray), hashtag);
     }
 
 }
