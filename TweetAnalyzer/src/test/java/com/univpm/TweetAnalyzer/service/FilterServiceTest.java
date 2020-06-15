4f30f51a3cbc067f5b49bab5081145ead6885596
Kevin Giannattasio <giannattasiok@yahoo.it>
2020-06-15 11:08:26 +0200

    risolto problema tests su json

--- TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/service/FilterServiceTest.java
+++ TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/service/FilterServiceTest.java
@@ -11,16 +11,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 class FilterServiceTest {
 
-    private static Object filter = "{\"Gesù\" : \"it\" }";
-    private Object IllegalFilterKeyException;
+    private static Map<String, String> filter = new LinkedHashMap<String, String>();
+    private Exception IllegalFilterKeyException;
 
     @Test
     @DisplayName("Verifica se il filtro è giusto")
     @Tag("Filter")
     void passedTest() {
-
+    	filter.put("Gesù","it");
         assertThrows(IllegalFilterKeyException.class, () -> FilterService.filterParsing(filter), "Il filtro immesso non è corretto");
     }
 }
