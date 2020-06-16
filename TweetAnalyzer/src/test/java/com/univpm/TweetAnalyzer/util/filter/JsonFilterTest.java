50cb8b6f86c14a5d797413c34142b7174e61dee1
antonet <antoniobaio90@gmail.com>
2020-06-16 12:23:20 +0200

    prova 2

--- TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/JsonFilterTest.java
+++ TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/JsonFilterTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompare;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
