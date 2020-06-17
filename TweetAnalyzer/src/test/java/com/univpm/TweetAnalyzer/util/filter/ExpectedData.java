8a5b32a1462cc8f9cdcacb02e7ac8a30a49c0d27
Kevin Giannattasio <giannattasiok@yahoo.it>
2020-06-17 14:39:10 +0200

    risolto problema tests con i filtri

--- TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/ExpectedData.java
+++ TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/ExpectedData.java
@@ -9,24 +9,31 @@ public class ExpectedData {
 
     private static Map<String, Map<Integer, Data>> expectedData = new HashMap<String, Map<Integer, Data>>();
 
-    public static Map<Integer, Data> innerExpectedMapFill(int index, Data data) {
-        Map<Integer, Data> innerExpectedMap = new HashMap<Integer, Data>();
-        innerExpectedMap.put(index, data);
-        return innerExpectedMap;
-    }
-
-    public static Map<String, Map<Integer, Data>> expectedDataInsertion() {
-
-        expectedData.put("Benetton", innerExpectedMapFill(9, new Data(
-                "it", "test", "Tue Jun 8 2:00:00 +0000 2020", 123, 200, 900, 700)));
-        expectedData.put("Benetton", innerExpectedMapFill(10, new Data(
-                "it", "test", "Tue Jun 7 1:00:00 +0000 2020", 123, 100, 1000, 800)));
-        return expectedData;
-    }
-
     public static Map<String, Map<Integer, Data>> getExpectedData() {
         return expectedDataInsertion();
     }
+    
+    public static Map<String, Map<Integer, Data>> expectedDataInsertion() {
 
+    	Map<Integer, Data> innerMap = new HashMap<Integer, Data>();
+    	
+    	innerMap.put(2, new Data(
+    			"it", "test", "Tue Jun 15 09:00:00 +0000 2020", 123, 900, 200, 700));
+    	innerMap.put(3, new Data(
+    			"it", "test", "Tue Jun 14 08:00:00 +0000 2020", 123, 800, 300, 600));
+    	
+        expectedData.put("Prada", innerMap);
+        expectedData.put("Prada", innerMap);
+        
+        Map<Integer, Data> emptyMap = new HashMap<Integer, Data>();
+    	
+        expectedData.put("Gucci", emptyMap);
+        expectedData.put("Benetton", emptyMap);
+        expectedData.put("Zegna", emptyMap);
+        expectedData.put("Trussardi", emptyMap);
+        expectedData.put("Valentino", emptyMap);
+        
+        return expectedData;
+    }
 
 }
--- TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/JsonFilterTest.java
+++ TweetAnalyzer/src/test/java/com/univpm/TweetAnalyzer/util/filter/JsonFilterTest.java
@@ -17,20 +17,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class JsonFilterTest {
 
     private static Map<String, Map<Integer, Data>> actualData = DataToTest.getDatasToTest();
-    private static ArrayList<LinkedHashMap<String, String>> filter = new ArrayList<LinkedHashMap<String,String>>();
+    private static ArrayList<LinkedHashMap<String, Object>> filter = new ArrayList<LinkedHashMap<String,Object>>();
 
-    public static LinkedHashMap<String, String> innerLinkedFill(String one, String two) {
-        LinkedHashMap<String, String> innerLinkedMap = new LinkedHashMap<String, String>();
-        innerLinkedMap.put(one, two);
+    public static LinkedHashMap<String, Object> innerLinkedFill(String key, Object val) {
+        LinkedHashMap<String, Object> innerLinkedMap = new LinkedHashMap<String, Object>();
+        innerLinkedMap.put(key, val);
         return innerLinkedMap;
     }
 
     @Test
     @DisplayName("Test su filtro di hashtag")
     void hashtagTest() throws DuplicateFilterException, IllegalFilterValueSizeException, IllegalFilterValueException, IllegalFilterKeyException, IllegalTimeException {
+    	ArrayList<String> val = new ArrayList<String>();
 
-        filter.add(innerLinkedFill("#", "Benetton"));
+    	val.add("12/06/2020 10:00");
+    	val.add("16/06/2020 11:00");
         filter.add(innerLinkedFill("Language", "it"));
+        filter.add(innerLinkedFill("Between", val));
 
         assertEquals(ExpectedData.getExpectedData(), FilterService.filterParsing(filter, actualData));
 
